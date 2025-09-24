c40bce6dbd9cdb3c55e292f33f6ad550f18b6d7b
cyruscampelo17 <cyrus.campelo@aluno.fiema.org.br>
2025-09-23 22:47:11 -0300

    Ocorrencias numero de matriz

---
+++ LISTA 2109/Ocorrenciasnumeromatriz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int contarOcorrencias(int matriz[4][4], int numero) {
+    int contador = 0;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (matriz[i][j] == numero) {
+                contador++;
+            }
+        }
+    }
+    return contador;
+}

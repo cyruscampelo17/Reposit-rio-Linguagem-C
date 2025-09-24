af86ddb32cc78ccb042d220011c938d4e9f512f6
cyruscampelo17 <cyrus.campelo@aluno.fiema.org.br>
2025-09-23 22:40:17 -0300

    Imprimir valores de matriz de usuarios

---
+++ LISTA 2109/imprimirvaloresmatrizusuarios.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int matriz[2][2];
+    
+
+    printf("Digite os valores da matriz 2x2:\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("matriz[%d][%d]: ", i, j);
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+    
+
+    printf("\nMatriz inserida:\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}

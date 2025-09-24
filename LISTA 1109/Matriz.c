833e0eefbc32edba0b740334ba70e9114a547f23
cyruscampelo17 <cyrus.campelo@aluno.fiema.org.br>
2025-09-23 22:30:05 -0300

    Matriz

---
+++ LISTA 1109/Matriz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int i, j, matriz[3][2];
+    for(i=0; i<3; i++){
+        for(j=0;j<2;j++){
+            printf("Digite um termo da matriz:");
+            scanf("%d", &matriz[3][2]);
+        }
+    }
+     for(i=0; i<3; i++){
+        for(j=0;j<2;j++){
+            printf("%d", matriz[i][j]);
+        }
+    }
+
+    return 0;
+}

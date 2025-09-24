25202b3db5e9b681aaa8e1c577f99efbf8d102c6
cyruscampelo17 <cyrus.campelo@aluno.fiema.org.br>
2025-09-23 22:53:26 -0300

    Soma de duas matrizes

---
+++ LISTA 2109/Somaduasmatrizes.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void somaMatrizes(int A[3][3], int B[3][3], int C[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
+}
+
+
+void imprimirMatriz(int matriz[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}

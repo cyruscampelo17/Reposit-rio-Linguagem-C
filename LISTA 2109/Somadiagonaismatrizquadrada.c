31a5447b8783748ef28673e3de39f07043ea0618
cyruscampelo17 <cyrus.campelo@aluno.fiema.org.br>
2025-09-23 22:56:35 -0300

    Soma da diagonais de uma matriz quadrada

---
+++ LISTA 2109/Somadiagonaismatrizquadrada.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int matriz[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    
+    int soma_principal = 0;
+    int soma_secundaria = 0;
+    
+    for (int i = 0; i < 3; i++) {
+        soma_principal += matriz[i][i];
+        soma_secundaria += matriz[i][2 - i];
+    }
+    
+    printf("Soma diagonal principal: %d\n", soma_principal);
+    printf("Soma diagonal secundaria: %d\n", soma_secundaria);
+    
+    return 0;
+}

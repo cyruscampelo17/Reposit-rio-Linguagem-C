0d19be42d21a8dd3dea8fd2b5b42c221d3f4806b
cyruscampelo17 <cyrus.campelo@aluno.fiema.org.br>
2025-09-23 22:54:07 -0300

    Soma elementos da linha de uma matriz

---
+++ LISTA 2109/Somaelementoslinha.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int somaLinha(int matriz[5][5], int linha) {
+    int soma = 0;
+    for (int j = 0; j < 5; j++) {
+        soma += matriz[linha][j];
+    }
+    return soma;
+}

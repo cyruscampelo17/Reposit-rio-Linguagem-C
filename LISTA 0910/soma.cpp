48fb5fb7489abe2c14580c46da2c5a12ed5c9bff
cyruscampelo17 <cyrus.campelo@aluno.fiema.org.br>
2025-10-11 14:15:03 -0300

    soma

---
+++ LISTA 0910/soma.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int n1, n2, soma;
+
+    cout <<"Qual o primeiro número?"<< endl;
+    cin >> n1;
+    cout <<"Qual o segundo número?"<< endl;
+    cin >> n2;
+
+    soma = n1 + n2;
+
+    cout <<"A soma do número: "<< n1 <<" e do número: "<< n2 <<" é igual a: "<< soma << endl;
+
+}
